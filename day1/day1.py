913364f110bca54d561577aea3b8d8e6ff66c677
Anzar-Akhtar <akhtaranzar6@gmail.com>
2026-09-13 15:25:32 +0530

    Starting NumPy

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# example 1
+marks = np.array([80,75, 90, 65, 88])
+print(marks)
+
+# example 2
+temp = np.array([32, 43, 33, 36, 30])
+print(temp)
+
+
+
+# 2D array
+
+marks_2D = ([ 
+    [80, 75, 90],
+    [60, 70, 46],
+    [95, 85, 92]
+])
+
+print(marks_2D)
+
+
+# Question
---
+++ day1/day1_prac_ques.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data = np.array([
+    [20, 1, 25000],
+    [25, 3, 40000],
+    [30, 5, 60000]
+])
+print(data)
